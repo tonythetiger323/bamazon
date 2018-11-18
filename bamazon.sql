522c5fc27b3b0c35db4a79894328a9da263249f7
Tony Greeley <tony.greeley@me.com>
2018-11-18 10:42:07 -0600

    add products to table

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,17 @@ CREATE TABLE products(
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Try Not To Suck", "Books", 20.00, 10), 
+("Nintendo Switch", "Electronics", 300.00, 10),
+("Pokemon: Let's Go, Pikachu!", "Electronics", 60.00, 10),
+("Couch", "Furniture", 500.00, 10),
+("Jeans", "Clothing", 60.00, 10),
+("Shoes", "Clothing", 50.00, 10),
+("Gloves", "Clothing", 10.00, 10),
+("Microwave", "Appliances", 150.00, 10),
+("Shower Curtain", "Domestics", 15.00, 10),
+("Charlie Brown Christmas Tree", "Holiday", 15.00, 10);
+
+SELECT * FROM products;
