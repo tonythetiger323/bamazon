b2c7d5d240445a64d41614c1b6ae19d2b5306779
Tony Greeley <tony.greeley@me.com>
2018-11-27 17:43:38 -0600

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,6 +22,7 @@ connection.connect((err) => {
     startApp();
 });
 
+//function to run to start the app
 const startApp = () => {
     console.log("Welcome to Bamazon, here's what we currently have availale:");
     connection.query("SELECT * FROM products",
@@ -33,18 +34,51 @@ const startApp = () => {
         }
     );
 
-    inquirer.prompt(
+    placeOrder();
+};
+
+const placeOrder = () => {
+    // inquirer prompt asking which product would like to buy and how many, inquirer validates to make sure input entered is a number.
+    inquirer.prompt([
         {
             name: "productId",
             type: "input",
-            message: "Please enter the ID of the product you would like to buy."
+            message: "Please enter the ID number of the product you would like to buy.",
+            validate: (value) => {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
         },
         {
             name: "quatity",
             type: "input",
-            message: "Please enter the quatity you would like to buy."
+            message: "Please enter the quatity you would like to buy.",
+            validate: (value) => {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
         }
-    )
-    connection.end();
-
+    ]).then((answer) => {
+        console.log(answer);
+        connection.query(`
+        SELECT product FROM bamazon WHERE item_id = ${answer.productID}`, (err, data) => {
+                if (err) {
+                    throw err;
+                } else if (data.stock_quantity <= 0) {
+                    console.log(`Insufficient quantity in stock! We currently have ${data.stock_quantity} in stock. Please try another order.`);
+                    startApp();
+                } else {
+                    console.log("Thank you for your purchase, please give us moment to process your order!");
+                    processOrder();
+                }
+            });
+    });
 };
+
+const processOrder = () => {
+
+}
